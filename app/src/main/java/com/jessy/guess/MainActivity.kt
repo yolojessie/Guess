9f3183a6744ddfe3a94d9ac935b8b8052aca4fe9
Jessy <jessie840904@gmail.com>
2020-11-24 11:27:54 +0800

    function design, toast, dialog

--- app/src/main/java/com/jessy/guess/MainActivity.kt
+++ app/src/main/java/com/jessy/guess/MainActivity.kt
@@ -2,12 +2,37 @@ package com.jessy.guess
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val secretNumber = SecretNumber()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "secret: " + secretNumber.secret)
+    }
 
+    fun check (view : View) {
+        var n = ed_number.text.toString().toInt()
+        Log.d("MainActivity", "ed_number " + n)
+        val diff :Int = secretNumber.validate(n)
+        var message = "Yes, you got it"
+        if (diff < 0) {
+            message = "Bigger"
+        } else if (diff > 0){
+            message = "Smaller"
+        }
+//        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        AlertDialog.Builder(this)
+                .setTitle("Message")
+                .setMessage(message)
+                .setPositiveButton("ok", null)
+                .show()
     }
 }
 
---
+++ app/src/main/java/com/jessy/guess/SecretNumber.kt
@@ -0,0 +1,17 @@
+package com.jessy.guess
+
+import java.util.*
+
+class SecretNumber {
+    val secret = Random().nextInt(10) + 1
+    var count = 0
+    fun validate(number : Int) : Int {
+        count ++
+        return number - secret
+    }
+}
+
+fun main() {
+    val secretNumber = SecretNumber()
+
+}
